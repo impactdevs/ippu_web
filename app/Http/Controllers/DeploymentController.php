09bfdfac47a74d82ab2fea50480d53729f59d319
Nsengiyumva wilberforce <nsengiyumvawilberforce@gmail.com>
2024-08-26 13:29:17 +0300

    deployment controller

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -2,9 +2,18 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Process;
 
 class DeploymentController extends Controller
 {
-    //
+    public function deploy()
+    {
+        $deployScript = realpath(public_path('deploy.sh'));
+
+        $process = shell_exec("bash $deployScript 2>&1");
+
+        \Log::info($process);
+
+        return response()->json(['message' => 'Deployment in progress']);
+    }
 }
