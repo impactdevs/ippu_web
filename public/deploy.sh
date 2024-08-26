d1a683c1458a1f765f5c34d0d140cef9e8fe575e
Nsengiyumva wilberforce <nsengiyumvawilberforce@gmail.com>
2024-08-26 17:07:59 +0300

    new update

--- public/deploy.sh
+++ public/deploy.sh
@@ -6,8 +6,8 @@ composer install --optimize-autoloader --no-dev >> /var/www/staging.ippu.org/sto
 php artisan migrate --force >> /var/www/staging.ippu.org/storage/logs/laravel.log 2>&1
 php artisan config:cache >> /var/www/staging.ippu.org/storage/logs/laravel.log 2>&1
 # compile assets with npm
-npm install >> /var/www/staging.ippu.org/storage/logs/laravel.log 2>&1
+sudo npm install >> /var/www/staging.ippu.org/storage/logs/laravel.log 2>&1
 # build assets
-npm run build >> /var/www/staging.ippu.org/storage/logs/laravel.log 2>&1
+sudo npm run build >> /var/www/staging.ippu.org/storage/logs/laravel.log 2>&1
 # log a  deployment success message
 echo "Deployment successful at $(date)" >> /var/www/staging.ippu.org/storage/logs/laravel.log
