5a2f0c58b60de99cc214db2c430c80749abaaa39
katznicho <katznicho@gamil.com>
2024-09-05 08:31:28 +0300

    fix:renamed table attendance to attendences

--- database/migrations/2024_09_03_125255_add_membership_number_to_attendance_table.php
+++ database/migrations/2024_09_03_125255_add_membership_number_to_attendance_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('attendances', function (Blueprint $table) {
+        Schema::table('attendences', function (Blueprint $table) {
             //
             $table->string('membership_number')->nullable();
         });
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('attendances', function (Blueprint $table) {
+        Schema::table('attendences', function (Blueprint $table) {
             //
             $table->dropColumn('membership_number');
         });
