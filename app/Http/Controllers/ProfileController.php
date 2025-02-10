a7612fbf67a504afc67e3e41eba5730df8202264
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-02-10 10:15:39 +0300

    certificate

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -95,107 +95,114 @@ class ProfileController extends Controller
             $file = $request->file('profile_pic');
             $extension = $file->getClientOriginalExtension();
 
-            $filename = time().rand(100,1000).'.'.$extension;
+            $filename = time() . rand(100, 1000) . '.' . $extension;
 
             $storage = \Storage::disk('public')->putFileAs(
-                        'profiles/',
-                        $file,
-                        $filename);
+                'profiles/',
+                $file,
+                $filename
+            );
 
             if (!$storage) {
                 return response()->json(['message' => 'Unable to upload profile pic!']);
-            }else{
+            } else {
                 $user->profile_pic = $filename;
             }
         }
         $user->save();
 
-        return redirect('profile')->with('success','Profile Details have been updated!');
+        return redirect('profile')->with('success', 'Profile Details have been updated!');
     }
 
-        //create a generate certificate helper function
-        public function generate_certificate_helper(User $user = null){
-            $manager = new ImageManager(new Driver());
+    //create a generate certificate helper function
+    public function generate_certificate_helper(User $user = null)
+    {
+        $manager = new ImageManager(new Driver());
 
-            $image = $manager->read(public_path('images/membership_template.jpeg'));
-            //get this year's 01/01
-            $yearStart = Carbon::now()->startOfYear()->format('dS F, Y');
+        $image = $manager->read(public_path('images/membership_template.jpeg'));
+        //get this year's 01/01
+        $yearStart = Carbon::now()->startOfYear()->format('dS F, Y');
 
-            //get this year's 31/12
-            $yearEnd = Carbon::now()->endOfYear()->format('dS F, Y');
+        //get this year's 31/12
+        $yearEnd = Carbon::now()->endOfYear()->format('dS F, Y');
 
-            $membershipProcessingDate = $yearStart;
+        $membershipProcessingDate = $yearStart;
 
-            //add 12 months to the processing date to get expiry date
-            $expiryDate = $yearEnd;
+        //add 12 months to the processing date to get expiry date
+        $expiryDate = $yearEnd;
 
-            $image->text(strtoupper($user->name), 890, 1150, function ($font) {
-                $font->filename(public_path('fonts/Roboto-Bold.ttf'));
-                $font->color('#405189');
-                $font->size(50);
-                $font->align('center');
-                $font->valign('middle');
-                $font->lineHeight(1.6);
-            });
+        $image->text(strtoupper($user->name), 890, 1150, function ($font) {
+            $font->filename(public_path('fonts/Roboto-Bold.ttf'));
+            $font->color('#405189');
+            $font->size(50);
+            $font->align('center');
+            $font->valign('middle');
+            $font->lineHeight(1.6);
+        });
 
-            $image->text($user->membership_number ?? "N/A", 1230, 1268, function ($font) {
-                $font->filename(public_path('fonts/Roboto-Bold.ttf'));
-                $font->color('#405189');
-                $font->size(50);
-                $font->align('center');
-                $font->valign('middle');
-                $font->lineHeight(1.6);
-            });
+        $image->text($user->membership_number ?? "N/A", 1230, 1268, function ($font) {
+            $font->filename(public_path('fonts/Roboto-Bold.ttf'));
+            $font->color('#405189');
+            $font->size(50);
+            $font->align('center');
+            $font->valign('middle');
+            $font->lineHeight(1.6);
+        });
 
-            $image->text(strtoupper($user->account_type->name), 900, 1490, function ($font) {
-                $font->filename(public_path('fonts/Roboto-Bold.ttf'));
-                $font->color('#405189');
-                $font->size(50);
-                $font->align('center');
-                $font->valign('middle');
-                $font->lineHeight(1.6);
-            });
+        $image->text(strtoupper($user->account_type->name), 900, 1490, function ($font) {
+            $font->filename(public_path('fonts/Roboto-Bold.ttf'));
+            $font->color('#405189');
+            $font->size(50);
+            $font->align('center');
+            $font->valign('middle');
+            $font->lineHeight(1.6);
+        });
 
-            $image->text($membershipProcessingDate, 1050, 1812, function ($font) {
-                $font->filename(public_path('fonts/Roboto-Bold.ttf'));
-                $font->color('#405189');
-                $font->size(50);
-                $font->align('center');
-                $font->valign('middle');
-                $font->lineHeight(1.6);
-            });
+        $image->text($membershipProcessingDate, 1050, 1812, function ($font) {
+            $font->filename(public_path('fonts/Roboto-Bold.ttf'));
+            $font->color('#405189');
+            $font->size(50);
+            $font->align('center');
+            $font->valign('middle');
+            $font->lineHeight(1.6);
+        });
 
-            $image->text($expiryDate, 1050, 1932, function ($font) {
-                $font->filename(public_path('fonts/Roboto-Bold.ttf'));
-                $font->color('#405189');
-                $font->size(50);
-                $font->align('center');
-                $font->valign('middle');
-                $font->lineHeight(1.6);
-            });
+        $image->text($expiryDate, 1050, 1932, function ($font) {
+            $font->filename(public_path('fonts/Roboto-Bold.ttf'));
+            $font->color('#405189');
+            $font->size(50);
+            $font->align('center');
+            $font->valign('middle');
+            $font->lineHeight(1.6);
+        });
 
-            $image->toPng();
+        $image->toPng();
 
-            //save the image to the public folder
-            $image->save(public_path('images/certificate-generated'.$user->id.'.png'));
+        //save the image to the public folder
+        $image->save(public_path('images/certificate-generated' . $user->id . '.png'));
 
-            return public_path('images/certificate-generated'.$user->id.'.png');
-        }
+        return public_path('images/certificate-generated' . $user->id . '.png');
+    }
 
     public function generate_membership_certificate()
     {
         $user = User::find(\Auth::user()->id);
         $certificate = $this->generate_certificate_helper($user);
+        // Ensure the file exists and is fully written
+        if (!file_exists($certificate) || filesize($certificate) == 0) {
+            return abort(500, 'Certificate generation failed.');
+        }
         return response()->download($certificate)->deleteFileAfterSend(true);
     }
-    public function email_membership_certificate(Request $request){
-        try{
-        $memberId = $request->input('member_id');
-        $user = User::find($memberId);
-        $certificate = $this->generate_certificate_helper($user);
-        \Mail::to($user->email)->send(new MembershipCertificate($certificate));
-        return redirect()->back()->with('success', __('Membership certificate has been sent to '.$user->email));
-        } catch(TransportException $exception){
+    public function email_membership_certificate(Request $request)
+    {
+        try {
+            $memberId = $request->input('member_id');
+            $user = User::find($memberId);
+            $certificate = $this->generate_certificate_helper($user);
+            \Mail::to($user->email)->send(new MembershipCertificate($certificate));
+            return redirect()->back()->with('success', __('Membership certificate has been sent to ' . $user->email));
+        } catch (TransportException $exception) {
             return redirect()->back()->withInput($request->input())->with('error', "Failed to email the certificate! Please try again later.");
         }
     }
