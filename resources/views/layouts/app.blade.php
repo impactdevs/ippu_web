2e5679d30072d1ffef7657e49258d716ac7e6ca1
Nsengiyumva Wilberforce <nsengiyumvawilberforce@gmail.com>
2025-04-08 11:20:04 +0300

    button disabling removed

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1256,9 +1256,13 @@
                     url: url,
                     success: function(data) {
                         console.log(data);
+
+                        // Sanitize the file name
+                        var sanitizedFileName = sanitizeFileName(data.name);
+
                         var a = document.createElement('a');
                         a.href = data.url;
-                        a.download = data.name;
+                        a.download = sanitizedFileName;
                         document.body.appendChild(a);
                         a.click();
                         document.body.removeChild(a);
@@ -1268,7 +1272,14 @@
                         console.log(data);
                     }
                 });
-            })
+            });
+
+            // Function to sanitize the filename
+            function sanitizeFileName(fileName) {
+                // Replace spaces with underscores, remove special characters, and ensure no leading/trailing spaces
+                return fileName.replace(/[\/\\?%*:|"<>]/g, '-').replace(/\s+/g, '_').trim();
+            }
+
 
             $(".datatable").DataTable({
                 dom: 'Bfrltip', // 'B' for buttons, 'f' for filtering, 'r' for processing, 't' for table, 'i' for info, 'p' for pagination
